68556d04b0a7c17a2c3ed1b4c9975de9d3ac04fb
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-02-26 00:50:41 -0500

    create new table

--- controllers/table.js
+++ controllers/table.js
@@ -3,7 +3,7 @@ const Table = require("../models/Table");
 
 const getTables = async (req, res = response) => {
   try {
-    const tables = await Table.find().populate("todo", "name", "status");
+    const tables = await Table.find().populate("todo", "name status");
     return res.status(200).json({
       ok: true,
       tables,
@@ -12,10 +12,31 @@ const getTables = async (req, res = response) => {
     console.log(error);
     res.status(500).json({
       ok: false,
-      msg: "Please speak to the administrator",
+      msg: "Talk to the administrator",
     });
   }
 };
+
+const createTable = async (req, res = response) => {
+    const table = new Table(req.body);
+    try {
+      table.todo = [];
+      const tableSaved = await table.save();
+  
+      res.status(201).json({
+        ok: true,
+        event: tableSaved,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        ok: false,
+        msg: "Talk to the administrator",
+      });
+    }
+}
+
+
 
 module.exports = {
   getTables,
