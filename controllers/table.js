81860da18464cec8989de5f0c105589de16d6cfd
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-02-26 01:09:28 -0500

    detele Table

--- controllers/table.js
+++ controllers/table.js
@@ -18,14 +18,41 @@ const getTables = async (req, res = response) => {
 };
 
 const createTable = async (req, res = response) => {
-    const table = new Table(req.body);
+  const table = new Table(req.body);
+  try {
+    table.todo = [];
+    const tableSaved = await table.save();
+
+    res.status(201).json({
+      ok: true,
+      event: tableSaved,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Talk to the administrator",
+    });
+  }
+};
+
+
+const deleteTable = async (req,res = response) => {
+    const tableId = req.params.id;
     try {
-      table.todo = [];
-      const tableSaved = await table.save();
+      const table = await Table.findById(tableId);
   
-      res.status(201).json({
+      if (!table) {
+        return res.status(404).json({
+          ok: false,
+          msg: "no table found with this id",
+        });
+      }
+
+      await Table.findByIdAndDelete(tableId);
+  
+      res.status(200).json({
         ok: true,
-        event: tableSaved,
       });
     } catch (error) {
       console.log(error);
@@ -35,9 +62,8 @@ const createTable = async (req, res = response) => {
       });
     }
 }
-
-
-
 module.exports = {
   getTables,
+  createTable,
+  deleteTable,
 };
