3831a1618f7e9e7554e5f806f8da03675b92a5e2
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-02-25 22:03:40 -0500

    create paths in the server file

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ dbConnection();
 // Parse json objects
 app.use(express.json())
 
+
+//Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/todos", require("./routes/todos"));
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is running in the port: ${process.env.PORT}`);
 })
