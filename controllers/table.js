5397ba7ac1ab2e91cafb63d72144719688024424
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-02-26 01:18:44 -0500

    delete todos

--- controllers/table.js
+++ controllers/table.js
@@ -36,66 +36,68 @@ const createTable = async (req, res = response) => {
   }
 };
 
+const deleteTable = async (req, res = response) => {
+  const tableId = req.params.id;
+  try {
+    const table = await Table.findById(tableId);
 
-const deleteTable = async (req,res = response) => {
-    const tableId = req.params.id;
-    try {
-      const table = await Table.findById(tableId);
-  
-      if (!table) {
-        return res.status(404).json({
-          ok: false,
-          msg: "no table found with this id",
-        });
-      }
-
-      await Table.findByIdAndDelete(tableId);
-  
-      res.status(200).json({
-        ok: true,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
+    if (!table) {
+      return res.status(404).json({
         ok: false,
-        msg: "Talk to the administrator",
+        msg: "no table found with this id",
       });
     }
-}
 
+    const todoIds = table.todos;
+
+    await Table.findByIdAndDelete(tableId);
+
+    await Todo.deleteMany({ _id: { $in: todoIds } });
+
+    res.status(200).json({
+      ok: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Talk to the administrator",
+    });
+  }
+};
 
 const editTable = async (req = request, res = response) => {
-    const tableId = req.params.id;
-    try {
-      const table = await Table.findById(tableId);
-  
-      if (!table) {
-        return res.status(404).json({
-          ok: false,
-          msg: "Table does not exist for this id",
-        });
-      }
-  
-      const newTable = {
-        ...req.body,
-      };
-  
-      const tableUpdate = await Table.findByIdAndUpdate(tableId, newTable, {
-        new: true,
-      });
-  
-      res.json({
-        ok: true,
-        evento: tableUpdate,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
+  const tableId = req.params.id;
+  try {
+    const table = await Table.findById(tableId);
+
+    if (!table) {
+      return res.status(404).json({
         ok: false,
-        msg: "Talk to the administrator",
+        msg: "Table does not exist for this id",
       });
     }
-  };
+
+    const newTable = {
+      ...req.body,
+    };
+
+    const tableUpdate = await Table.findByIdAndUpdate(tableId, newTable, {
+      new: true,
+    });
+
+    res.json({
+      ok: true,
+      evento: tableUpdate,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Talk to the administrator",
+    });
+  }
+};
 
 module.exports = {
   getTables,
