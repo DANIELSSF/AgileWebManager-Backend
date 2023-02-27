b983b9894411d4e61481d5a51907235c23fb636f
DANIELSSF <daniel.santiago730@gmail.com>
2023-02-27 14:28:37 -0500

    The find of the getTables function has been updated.

--- controllers/table.js
+++ controllers/table.js
@@ -3,7 +3,7 @@ const Table = require("../models/Table");
 
 const getTables = async (req, res = response) => {
   try {
-    const tables = await Table.find().populate("todo", "name status");
+    const tables = await Table.find();
     return res.status(200).json({
       ok: true,
       tables,
