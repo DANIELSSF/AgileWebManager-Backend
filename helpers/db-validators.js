cb8908175734a49c8a67b1ca988947a495c0415a
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-06-09 20:59:02 -0500

    Added error handling functions to check the existence of various entities

---
+++ helpers/db-validators.js
@@ -0,0 +1,46 @@
+const Comment = require('../models/Comment');
+const Table = require('../models/Table');
+const Todo = require('../models/Todo');
+const User = require('../models/User');
+
+const checkExistsById = async (model, id, errorMessage) => {
+  const item = await model.findById(id);
+  if (!item) {
+    throw new Error(errorMessage);
+  }
+};
+
+const checkCommentExistsById = async (commentId) => {
+  await checkExistsById(Comment, commentId, 'No comment exists for this id');
+};
+
+const checkTodoExistsById = async (todoId) => {
+  await checkExistsById(Todo, todoId, 'No todo exists for this id');
+};
+
+const checkTableExistsById = async (tableId) => {
+  await checkExistsById(Table, tableId, 'No table exists for this id');
+};
+
+const checkUserExistsById = async (userId) => {
+  await checkExistsById(User, userId, 'No user with this id was found');
+};
+
+const checkUserExistsByEmail = async (email) => {
+  const user = await Table.findOne({ email });
+  if (user) {
+    throw new Error('User exists with that email address');
+  }
+};
+
+
+const validStatus = new Set(['start', 'progress', 'done']);
+
+module.exports = {
+  checkCommentExistsById,
+  checkTodoExistsById,
+  checkTableExistsById,
+  validStatus,
+  checkUserExistsByEmail,
+  checkUserExistsById,
+};
